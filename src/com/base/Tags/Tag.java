bfb0b426f4457ffdab8e966a258583cb1b180dba
Grz3hu <grek222@o2.pl>
2023-01-08 19:42:59 +0100

    added tag iterator

--- src/com/base/Tags/Tag.java
+++ src/com/base/Tags/Tag.java
@@ -3,11 +3,16 @@ package com.base.Tags;
 import com.base.Notes.NoteInterface;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
-public abstract class Tag {
+public abstract class Tag implements Iterable {
     private final ArrayList<NoteInterface> noteList = new ArrayList<>();
 
     public ArrayList<NoteInterface> getNoteList(){
         return this.noteList;
+    }
+
+    public Iterator iterator() {
+        return new TagIterator(this);
     }
 }
