8ee6d7d07f78d1c1e5a22cc89812fadf54d495b7
Grz3hu <grek222@o2.pl>
2023-01-15 22:01:45 +0100

    added maven, junit and example test

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,13 @@
+import com.base.Users.User;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class ExampleTest {
+
+    @Test
+    public void testUser(){
+        User t = new User("d","123");
+        assertNotNull(t);
+    }
+}
